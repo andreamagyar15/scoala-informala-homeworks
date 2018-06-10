2c1e2d5eb593e4c5fbbb18270edbc5119ec6b88f
andreamagyar15 <andreamagyar15@gmail.com>
2018-06-10 20:37:42 +0300

    gitignore

---
+++ PackageDeliverySystem/src/main/java/ro/siit/java/packageDelivery/Address.java
@@ -0,0 +1,14 @@
+package ro.siit.java.packageDelivery;
+
+public class Address {
+    private String country;
+    private String city;
+    private String street;
+    private String zipCode;
+    public Address(String country,String city, String street,String zipCode){
+        this.country=country;
+        this.city=city;
+        this.street=street;
+        this.zipCode=zipCode;
+    }
+}
