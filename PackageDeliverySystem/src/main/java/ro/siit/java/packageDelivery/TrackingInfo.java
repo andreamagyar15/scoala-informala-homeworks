f2ec216b42f7cd65361a0304965633ac6ebae32b
andreamagyar15 <andreamagyar15@gmail.com>
2018-06-10 20:34:02 +0300

    gitignore

--- PackageDeliverySystem/src/main/java/ro/siit/java/packageDelivery/TrackingInfo.java
+++
@@ -1,8 +0,0 @@
-package ro.siit.java.packageDelivery;
-
-public class TrackingInfo {
-    private String packageHistory;
-    public String addInfoToHistory(String trakingId){
-        return null;
-    }
-}
