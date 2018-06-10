1ca0a14de1b881b18852259b76d716485e820a49
andreamagyar15 <andreamagyar15@gmail.com>
2018-06-10 19:12:22 +0300

    commit desing

---
+++ PackageDeliverySystem/src/main/java/ro/siit/java/packageDelivery/PackageDeliverySystem.java
@@ -0,0 +1,23 @@
+package ro.siit.java.packageDelivery;
+
+public class PackageDeliverySystem {
+
+    private Parcel packages[];
+    private Facility facility;
+
+    public String requestDeliveryPickup(ContactInfo pickupContactInfo, ContactInfo deliveryContactInfo) {
+
+        return null;
+    }
+    public TrackingInfo getTrackingInfo(String trackingId){
+
+        return null;
+    }
+    public Parcel[] getPackages(Administrator admin){
+
+        return null;
+    }
+    public String[] routCalculation(ContactInfo pickupAddress,ContactInfo deliveryAddress){
+        return null;
+    }
+}
---
+++ PackageDeliverySystem/src/main/java/ro/siit/java/packageDelivery/TrackingInfo.java
@@ -0,0 +1,8 @@
+package ro.siit.java.packageDelivery;
+
+public class TrackingInfo {
+    private String packageHistory;
+    public String addInfoToHistory(String trakingId){
+        return null;
+    }
+}
